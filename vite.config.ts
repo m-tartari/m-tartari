a74ac681694313f3c27adfee19413c80c7fccb03
m-tartari <37861893+m-tartari@users.noreply.github.com>
2024-01-20 09:46:29 +0100

    ✅ test: add vitest.setup.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   test: {
-    environment: 'jsdom'
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./vitest.setup.ts']
   }
 })
