2ddfd69b38c556fa77d20e949942d15b99f43160
m-tartari <37861893+m-tartari@users.noreply.github.com>
2024-01-20 11:06:51 +0100

    ➕ chore(deps-dev): add dependency vite-tsconfig-paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react-swc'
+import viteTsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteTsconfigPaths(),
+  ],
   server: {
     open: true,
     host: true,
