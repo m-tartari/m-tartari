8aff802faa3c464150258203d7b6141f0f53fcf4
m-tartari <37861893+m-tartari@users.noreply.github.com>
2024-01-20 16:07:05 +0100

    ♻️ refactor(App): use custom theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ThemeProvider } from '@mui/material/styles'
+import { CssBaseline } from '@mui/material'
+
 import App from './App.tsx'
+import theme from 'style/theme.ts'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 )
---
+++ src/style/theme.ts
@@ -0,0 +1,20 @@
+import { createTheme } from '@mui/material/styles'
+
+// A custom theme for this app
+const theme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#e44c65',
+    },
+    secondary: {
+      main: '#3f51b5',
+    },
+    background: {
+      default: '#101018',
+      paper: '#1c1d26',
+    },
+  },
+})
+
+export default theme
