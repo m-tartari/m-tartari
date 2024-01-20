7fa16a0c9909b5507acd603bd3302086c205a07f
m-tartari <37861893+m-tartari@users.noreply.github.com>
2024-01-20 10:28:02 +0100

    🔧 chore(vite): open browser on start

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    open: true,
+    host: true,
+  },
   test: {
     globals: true,
     environment: 'jsdom',
