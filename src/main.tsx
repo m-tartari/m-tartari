b19148ad8f41c8177f44d063e1f90b2277647adf
m-tartari <37861893+m-tartari@users.noreply.github.com>
2024-01-22 18:45:02 +0100

    ♻️ refactor(App): make explicit root element

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,8 @@ import App from './App.tsx'
 import theme from 'style/theme.ts'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <CssBaseline />
