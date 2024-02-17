b6ebee68928702995726422a534b35689cf4a8f5
m-tartari <37861893+m-tartari@users.noreply.github.com>
2024-02-17 10:35:32 +0100

    ♻️ refactor(atoms): update export index

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -1,2 +1,6 @@
+export { default as Award } from './Award'
+export { default as Extracurricular } from './Extracurricular'
+export { default as Publication } from './Publication'
+
 export { default as IconLinks } from './IconButton'
 export * from './IconLinks'
