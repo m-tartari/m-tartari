35dc2a45a97d873f10e624751806a78f7db094f5
m-tartari <37861893+m-tartari@users.noreply.github.com>
2024-02-17 14:35:16 +0100

    🔧 chore(vitest): reset mocks and cleanup after each test

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -3,3 +3,13 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom'
+
+/**
+ * Reset mocks and cleanup after each test
+ */
+import { cleanup } from '@testing-library/react'
+import { vi, afterEach } from 'vitest'
+afterEach(() => {
+  vi.resetAllMocks()
+  cleanup()
+})
