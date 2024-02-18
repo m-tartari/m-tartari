de164affca3f25ce1673b588a985211cc8f391d5
m-tartari <37861893+m-tartari@users.noreply.github.com>
2024-02-18 15:16:00 +0100

    ♻️ refactor(home): add project spotlight mobile version

--- src/components/pages/MainPage/MainPage.test.tsx
+++ src/components/pages/MainPage/MainPage.test.tsx
@@ -5,9 +5,23 @@ import { renderWithRouter as render } from 'components/utils/tests'
 import MainPage from '.'
 
 // Tests
-describe('Main page', async () => {
-  it('Should render the page correctly', async () => {
-    // Setup
+describe('Main page', () => {
+  it('Should render the page correctly on desktop', async () => {
+    await render(<MainPage />)
+
+    // Make sure the page title is correct
+    expect(screen.getByText('Robotics Engineer')).toBeInTheDocument()
+
+    // Make sure the project spotlights are present
+    expect(screen.getByText('LINFA: An Intuitive Block-Programming Interface for Reconfigurable Robots')).toBeInTheDocument()
+    expect(screen.getByText("Revamping HHCM's Modular: Swift Generation of Modular Robot Models")).toBeInTheDocument()
+    expect(screen.getByText('Automated Actuators and Robotic Modules Calibration')).toBeInTheDocument()
+    expect(screen.getByText('Voice Controlled Mobile Robot for Indoor Applications')).toBeInTheDocument()
+  })
+  it('Should render the page correctly on mobile', async () => {
+    // Set the window size to mobile (Galaxy Note 20 Android 11)
+    window.innerWidth = 412
+    window.innerHeight = 915
     await render(<MainPage />)
 
     // Make sure the page title is correct
