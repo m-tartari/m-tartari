4a0fa4a486955b02db9002d031667ec099c9314d
m-tartari <37861893+m-tartari@users.noreply.github.com>
2024-12-04 15:04:18 +0000

    ♻️ refactor: fix production build

---
+++ vitest.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      components: '/src/components',
+      style: '/src/style',
+    },
+  },
+  test: {
+    watch: false,
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/setupTests.ts'],
+    coverage: {
+      provider: 'istanbul',
+      include: ['src'],
+      exclude: ['./**/*.d.ts', './src/*.ts', './**/mocks/*.*', './**/{m,M}ock*.*', './**/*.test.tsx', './**/*.stories.*'],
+    },
+  },
+})
