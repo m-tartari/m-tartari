6dcc0c06a7745313318c26a6245bfd1f083e38ee
m-tartari <37861893+m-tartari@users.noreply.github.com>
2025-05-23 14:19:36 +0000

    ♻️ refactor(atoms): cleanup unused files

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -2,5 +2,4 @@ export { default as Award } from './Award'
 export { default as Extracurricular } from './Extracurricular'
 export { default as Publication } from './Publication'
 
-export { default as IconLinks } from './IconButton'
 export * from './IconLinks'
