8ba9e78a16e00ba02b8ffae24e38b70c412cbb26
m-tartari <37861893+m-tartari@users.noreply.github.com>
2025-07-03 10:32:14 +0200

    ✨ feat(card): add Business Card page
    adds a page with print functionality

--- src/components/pages/index.ts
+++ src/components/pages/index.ts
@@ -2,5 +2,6 @@ export { default as MainPage } from './MainPage'
 export { default as ContactPage } from './ContactPage'
 export { default as CurriculumPage } from './CurriculumPage'
 export { default as ProjectsPage } from './ProjectsPage'
+export { default as BusinessCardPage } from './BusinessCardPage'
 
 export { default as ErrorPage } from './ErrorPage'
