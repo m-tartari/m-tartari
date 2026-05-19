32b75ad31a0389d17091e5a2bc5699769468984f
m-tartari <37861893+m-tartari@users.noreply.github.com>
2026-05-19 18:39:13 +0200

    ➖ chore(deps-dev): remove deprecated dependency vite-tsconfig-paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import viteTsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteTsconfigPaths()],
+  plugins: [react()],
   resolve: {
+    tsconfigPaths: true,
     conditions: ['mui-modern', 'module'],
     alias: {
       components: '/src/components',
